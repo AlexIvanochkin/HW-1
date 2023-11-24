10aa98ce503441d8be9e1f161cf8bc4f1811c912
Alex Test <mr.angryjoker@gmail.com>
2023-11-24 23:24:08 +0300

    changed names2

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Вы получили бонусы:");
 
-        int ticket_price = 1700; //цена билета
-        int bonus_rubles = 20; // рублей для получения 1 бонусной мили
-        int solution = Math.round(ticket_price / 20);
+        int ticketPrice = 1750; //цена билета
+        int bonusRubles = 20; // рублей для получения 1 бонусной мили
+        int solution = Math.round(ticketPrice / 20);
 
-        if (ticket_price > 20) {
+        if (ticketPrice > 20) {
             System.out.println(solution);
-        } else if (ticket_price == 20) {
+        } else if (ticketPrice == 20) {
             System.out.println(solution);
         } else {
             System.out.println("0");
